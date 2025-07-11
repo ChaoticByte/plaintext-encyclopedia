89fbb17ab7d9aa4d93c67ae09587f1a7418dbdb3
ChaoticByte <contact@chaoticbyte.net>
2025-07-11 21:32:16 +0200

    Require .txt filename extension for entries, allow defining entry title via Title: ... in first line of the file, update examples

--- README.md
+++ README.md
@@ -8,11 +8,24 @@ A very simple and clean reader/browser? for plaintext... documentation? wiki? ch
 ## How to use
 
 1. Set your settings in `settings.go`
-2. Modify (if you want)
+2. Modify the application (if you want)
 3. Compile `go get && go build -o server`
-4. Place your content into `entries/`
+4. Place your `.txt` files into `entries/` (see section "Formatting" below)
 5. Run `./server`
 6. put behind a proxy (or don't).
+
+### Formatting
+
+The only formatting option you have: to define a title.
+
+```
+Title: A title
+
+here comes the entry body ...
+```
+
+It has to be defined in the first line of the text file.  
+If you don't define a title, the filename without the `.txt` part gets used as the title.
 
 ## Requirements
 
--- database.go
+++ database.go
@@ -44,20 +44,44 @@ func (db *Database) search(query string) []string { // returns keys (entry names
 func BuildDB(directory string) Database {
 	logger := log.Default()
 	logger.Println("Building database")
-	// keys, entries
-	var keys []string
+	// files, entries
+	var files []string
 	entries := map[string]string{}
 	// get files in directory and read them
 	directory = strings.TrimRight(directory, "/") // we don't need that last /, don't use the root directory /
 	entriesDirFs := os.DirFS(directory)
-	keys, err := fs.Glob(entriesDirFs, "*")
+	files, err := fs.Glob(entriesDirFs, "*.txt")
 	if err != nil { logger.Panicln(err) }
-	for _, k := range keys {
-		contentB, err := os.ReadFile(directory + "/" + k)
+	titles := []string{}
+	for _, f := range files {
+		fileData, err := os.ReadFile(directory + "/" + f)
 		if err != nil { logger.Panicln(err) }
-		content := string(contentB)
-		entries[k] = content
+		content := string(fileData)
+		content = strings.Trim(content, "\n\r ")
+		if strings.HasPrefix(content, "Title:") {
+			len_content := len(content)
+			title_start_idx := 6
+			var title_stop_idx int
+			if strings.Contains(content, "\n") {
+				title_stop_idx = strings.Index(content, "\n")
+			} else {
+				title_stop_idx = len_content
+			}
+			title := content[title_start_idx:title_stop_idx]
+			title = strings.Trim(title, " ")
+			body := content[title_stop_idx:len_content]
+			body = strings.TrimLeft(body, "\n\r")
+			if len(body) < 1 {
+				body = " "
+			}
+			titles = append(titles, title)
+			entries[title] = body
+		} else {
+			title := f[:len(f)-4] // remove ".txt"
+			titles = append(titles, title)
+			entries[title] = content
+		}
 	}
 	matcher := search.New(ContentLanguage, search.IgnoreCase, search.IgnoreDiacritics)
-	return Database{Keys: keys, Entries: entries, matcher: matcher}
+	return Database{Keys: titles, Entries: entries, matcher: matcher}
 }
